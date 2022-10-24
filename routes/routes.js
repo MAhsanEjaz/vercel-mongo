22004f0c3a8793a87b0b41e00307dc6638a5d6c0
ahsan <ahsanejaz99@gmail.com>
2022-10-24 15:29:37 +0500

    qw

--- routes/routes.js
+++ routes/routes.js
@@ -9,31 +9,27 @@ app.use(express.static('uploads'));
 
 
 
-const storage = multer.diskStorage({
-    destination: function(req, file, cb){
-     cb(null, './uploads/')
-    },
-    filename: function(req, file, cb) {
-     cb(null, Date.now() + file.originalname);
-   }
- });
- 
-//  const filter = (req, file , cb)=>{
-//      if(file.mimetype == 'image/jpeg' || file.mimetype == 'image/png'){
-//          cb(null, true);
-//      }else{
-//          cb(null, false);
-//      }
-//  }
 
-const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 1024 * 1024 * 5
+const storage = multer.diskStorage({
+  destination: function(req, file, cb){
+   cb(null, './uploads/')
   },
-  // fileFilter: filter
+  filename: function(req, file, cb) {
+   cb(null, Date.now() + file.originalname);
+ }
 });
 
+// const filter = (req, file , cb)=>{
+//     if(file.mimetype == 'image/jpeg' || file.mimetype == 'image/png'  || file.mimetype == image/gif){
+//         cb(null, true);
+//     }else{
+//         cb(null, false);
+//     }
+// }
+const upload = multer({
+   storage : storage, 
+   // fileFilter: filter
+}).single('image');
 
 
 
@@ -41,36 +37,39 @@ const upload = multer({
 
 
 
-app.post("", upload.single('image'), (req, res, next) => {
-    const product = new image({
-      // _id: new mongoose.Types.ObjectId(),
-      name: req.body.name,
-      price: req.body.price,
-      productImage: req.file.filename
-    });
-    product
-      .save()
-      .then(result => {
-        console.log(result);
-        res.status(201).json({
-          message: "Created product successfully",
-          createdProduct: {
-              // name: result.name,
-              // price: result.price,
-              // _id: result._id,
-              request: {
-                  type: 'GET',
-                  url: "http://localhost:1000/products/" + result._id
-              }
-          }
-        });
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json({
-          error: err
-        });
-      }); });
+
+
+app.post("", upload, (req, res, next) => {
+  const product = new products({
+    // // _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    price: req.body.price,
+    productImage: req.file.filename
+  });
+  product
+    .save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: "Created product successfully",
+        createdProduct: {
+            // name: result.name,
+            // price: result.price,
+            // _id: result._id,
+            request: {
+                type: 'GET',
+                url: "http://localhost:1000/products/" + result._id
+            }
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    }); });
+
 
 
       
